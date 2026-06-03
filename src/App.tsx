902f247b8711b239254dfac11f6b024145e5064f
José Alonso <jose.lopez.car@tecsup.edu.pe>
2026-06-03 16:49:10 -0500

    Great

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Entities from "./pages/Entities";
